1e7c14a95bc23a6cdc730aeaecdeffff617f895f
FairFightKing <sosthenefruchard@gmail.com>
2020-11-22 21:17:16 +0000

    Create Read Update all set up plus readme"

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # BackEnd_CA1
-# DB Password CD2CK55yScXVzij
 So we first start our project with a little command "> index.js" to create an empty index.js file. 
 We first have to git init inside our project folder and add the project remote. 
 When that's done, and we have one our first init commit, we first init our node app running a "npm init" since we already have installed node and npm. 
@@ -8,6 +7,12 @@ After doing all that we should first gitignore the node_modules because we don't
 
 So first we have to start our index.js file with the dependencies we need, so we require express and set up the port we will listen on for later. 
 Then we create the Mongo database and create a cluster then create a db and a collection. After that we import it into our code and create a user password.
+We also need to set up our connection to the db and setup the app.listen to have our app listening on the right port and connecting to the db plus having the corresponding collection.
 Then we have to set up our CRUD by initialising each CRUD operation.
 
-Then we want to define our object that we are going to use and store inside our database, so we code it to use it as a model for our database and CRUD operations.
+Then we want to define our object that we are going to use and store inside our database, so we code it to use it as a model for our database and CRUD operations.And I just saw that we don't use Mongoose, so we can't use a model for our database.
+To try the connection to our db we have to set up our READ getter to see if we can ask for the whole database as a json.
+I also wanted to not use that async func that we launch without argument, I really think we could have done better, but we didn't see any other way to do it, so I'll stick to this. 
+Same for the run func that I would prefer to use the app.listen function to do that but I don't know if that's possible.
+Inside the Read I made it so if we don't use any ID it will return the whole database and if we put any ID it will look for a matching one and send it to the user.
+Plus I used ternary operator to simplify the Update, so we can put any field we want, and it will keep the ones we didn't specify;
--- index.js
+++ index.js
@@ -5,14 +5,100 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false}));
 // Mongo
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
 const uri = "mongodb+srv://FFKing:CD2CK55yScXVzij@cluster0.vypn2.mongodb.net/CardsDB?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true});
+
 // DB INFOS
 const port = 3000;
 const dbName = "CardsDB";
+const colName = "Hearthstone";
+let collection;
+// we run our app which func is written at the end of the file
+run();
+// HomePage
+app.get('/', (req,res) => {
+    res.send('Please use /hs thanks.')
+})
+// READ
+app.get('/hs', (req, res)  => {
+    if(req.body.id) {
+        async function getCard() {
+            try{
+                let cardData = await collection.findOne({"_id": ObjectId(req.body.id)})
+                res.json(cardData).status(200)
+            } catch{
+                res.send('Could not find specified ID').status(400)
+            }
+        }
+        getCard();
+    } else {
+        async function getCards () {
+            let CardsData = await collection.find().toArray()
+            res.json(CardsData).status(200)
+        }
+        getCards();
+    }
 
+})
+// CREATE
+app.post('/hs', (req, res) => {
+    let NewCard = new HsCard(req.body.Name,req.body.Mana,req.body.Golden,req.body.Attack,req.body.HP,req.body.Keyword)
+    collection.insertOne(NewCard).then(() => res.status(200)).catch(() => res.status(400));
+})
+// UPDATE
+app.put('/hs', (req, res) => {
+    let card;
+    async function findCard(){
+        card = await collection.findOne({"_id": ObjectId(req.body.id)})
+        if(card !== null){
+            let UpdateCard = new HsCard(
+                req.body.Name ? req.body.Name : card.Name,
+                req.body.Mana ? req.body.Mana : card.Mana,
+                req.body.Golden ? req.body.Golden : card.Golden,
+                req.body.Attack ? req.body.Attack : card.Attack,
+                req.body.HP ? req.body.HP : card.HP,
+                req.body.Keyword ? req.body.Keyword : card.Keyword,
+            )
+            try {
+                await collection.updateOne({"_id": ObjectId(req.body.id)},{$set:UpdateCard})
+                res.status(200).send('Update Successful')
+            } catch (e) {
+                res.send('Update Failed But card found').status(400)
+            }
+        } else{
+            res.send('card not found,no matching id').status(400)
+        }
+    }
+    findCard();
+})
 
-// We listen on port 3000
-app.listen(port, function() {
-    console.log(`listening on ${port}`);
-});
+// connection to DB
+async function run(){
+    try {
+        await client.connect(() => {
+            collection = client.db(dbName).collection(colName);
+            app.listen(port);
+            console.log(`listening on ${port}.`)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+class HsCard{
+    Name;
+    Mana;
+    Golden;
+    HP;
+    Keyword;
+    Attack;
+    constructor(Name,Mana,Golden = false, Attack,HP,Keyword = 'none') {
+        this.Name = Name;
+        this.Mana = Mana;
+        this.Golden = Golden;
+        this.Attack = Attack;
+        this.HP = HP;
+        this.Keyword = Keyword;
+    }
+}
